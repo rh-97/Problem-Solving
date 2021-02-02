af71d36c3e961a46eccd2a36b161ec3c987c65f8
rh-97 <52336887+rh-97@users.noreply.github.com>
2021-02-02 21:31:54 +0600

    Bit manipulation

--- 191. Number of 1 Bits.cpp
+++ 191. Number of 1 Bits.cpp
@@ -10,7 +10,7 @@
     It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
     In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3 above, the input represents the signed integer. -3.
     Follow up: If this function is called many times, how would you optimize it?
-
+    
 
     Example 1:
     Input: n = 00000000000000000000000000001011
