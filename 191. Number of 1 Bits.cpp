73b664cc82997133ee5a2702081d25facbe03642
rh-97 <52336887+rh-97@users.noreply.github.com>
2021-02-03 16:10:55 +0600

    Bit manipulation - O(1)

--- 191. Number of 1 Bits.cpp
+++ 191. Number of 1 Bits.cpp
@@ -46,11 +46,11 @@
 /* Explanation
 
 We can make the previous algorithm simpler and a little faster. Instead of checking every bit of the number, 
-we repeatedly flip the least-significant 11-bit of the number to 00, and add 11 to the sum. As soon as the number becomes 00, 
-we know that it does not have any more 11-bits, and we return the sum.
+we repeatedly flip the least-significant 1-bit of the number to 0, and add 1 to the sum. As soon as the number becomes 0, 
+we know that it does not have any more 1-bits, and we return the sum.
 
-The key idea here is to realize that for any number n, doing a bit-wise AND of n and n - 1 flips the least-significant 11-bit in nn to 00. 
-Why? Consider the binary representations of nn and n - 1.
-In the binary representation, the least significant 11-bit in nn always corresponds to a 00-bit in n - 1n−1. Therefore, anding the two numbers n and 
-n - 1 always flips the least significant 11-bit in nn to 00, and keeps all other bits the same.
+The key idea here is to realize that for any number n, doing a bit-wise AND of n and n - 1 flips the least-significant 1-bit in n to 0. 
+Why? Consider the binary representations of n and n - 1.
+In the binary representation, the least significant 1-bit in n always corresponds to a 0-bit in n - 1. Therefore, anding the two numbers n and 
+n - 1 always flips the least significant 1-bit in n to 0, and keeps all other bits the same.
 */
