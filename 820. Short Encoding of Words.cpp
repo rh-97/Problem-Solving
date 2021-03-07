87df3c300d7feff1c3ea5333ada47054ff7da76a
rh-97 <52336887+rh-97@users.noreply.github.com>
2021-03-07 06:34:37 +0600

    Trie - O(sum(len(word) for word in words))

---
+++ 820. Short Encoding of Words.cpp
@@ -0,0 +1,79 @@
+/* PROBLEM STATEMENT
+
+A valid encoding of an array of words is any reference string s and array of indices indices such that:
+
+words.length == indices.length
+The reference string s ends with the '#' character.
+For each index indices[i], the substring of s starting from indices[i] and up to (but not including) the next '#' character is 
+equal to words[i].
+Given an array of words, return the length of the shortest reference string s possible of any valid encoding of words.
+
+ 
+
+Example 1:
+
+Input: words = ["time", "me", "bell"]
+Output: 10
+Explanation: A valid encoding would be s = "time#bell#" and indices = [0, 2, 5].
+words[0] = "time", the substring of s starting from indices[0] = 0 to the next '#' is underlined in "time#bell#"
+words[1] = "me", the substring of s starting from indices[1] = 2 to the next '#' is underlined in "time#bell#"
+words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '#' is underlined in "time#bell#"
+Example 2:
+
+Input: words = ["t"]
+Output: 2
+Explanation: A valid encoding would be s = "t#" and indices = [0].
+
+ 
+
+Constraints:
+
+1 <= words.length <= 2000
+1 <= words[i].length <= 7
+words[i] consists of only lowercase letters.
+
+*/
+
+// SOLUTION
+// Trie
+// 
+class Solution {
+public:
+    
+    int total = 0;
+
+    struct Trie {
+        Trie () {
+            children.resize(26);
+        }
+        vector<Trie*> children;
+        bool f = true;
+        void insert(string &word, int n) {  // O(n), here n is the word length
+            if (n >= 0) {
+                f = false;
+                if (!children[word[n] - 'a']) 
+                    children[word[n] - 'a'] = new Trie();
+                children[word[n] - 'a']->insert(word, n - 1);
+            }
+        }
+    };
+    
+    void calculate(Trie *root, int n) {    // BFS - O(sum(len(word) for word in words))
+        if (root->f) {
+            total += n + 1; return;
+        }
+        for (Trie *child : root->children) {
+            if (child) calculate(child, n + 1);
+        }
+    }
+    
+    int minimumLengthEncoding(vector<string>& words) {
+        Trie *root = new Trie();
+        for (string &word : words) {        // building the Trie structure
+            root->insert(word, word.size() - 1);
+        }
+        calculate(root, 0);   // calculating total of all root to lleaf length in the Trie structure
+        return total;
+    }
+};
+
