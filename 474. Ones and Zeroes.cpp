01018c4bbb5c2fbf35923f20dc6aae1977b8f19d
rh-97 <52336887+rh-97@users.noreply.github.com>
2021-04-02 21:52:38 +0600

    DP - O(L * m * n)

---
+++ 474. Ones and Zeroes.cpp
@@ -0,0 +1,23 @@
+// DP
+// O(L * m * n), L -> length of strs.
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1));
+        for (string &str : strs) {
+            int ones = count(str.begin(), str.end(), '1'), zeros = str.size() - ones;
+            for (int i = m; i >= zeros; --i) {
+                for (int j = n; j >= ones; --j) {
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+
+
+
+
+
