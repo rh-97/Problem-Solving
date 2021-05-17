8ef552429da00c898fa1f9c065e868e48be979cd
rh-97 <52336887+rh-97@users.noreply.github.com>
2021-05-17 20:12:40 +0600

    DP - O(L^2 * N)

--- 1048. Longest String Chain.cpp
+++ 1048. Longest String Chain.cpp
@@ -46,3 +46,73 @@ private:
 // DP - Top-Bottom
 // O(N * (log N + L^2))
 
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        
+        vector<vector<pair<string, int>>> dp(17);
+        for (string &word : words) {
+            dp[word.length()].push_back({word, 1});
+        }
+        
+        int max_chain = 0;
+        
+        for (int len = 2; len < 17; ++len) {
+            for (auto &[word, chain] : dp[len]) {
+                for (auto &[pred, pred_chain] : dp[len - 1]) {
+                    if (predecessor(pred, word)) {
+                        chain = max(chain, 1 + pred_chain);
+                    }
+                }
+                max_chain = max(max_chain, chain);
+            }
+        }
+        
+        return max_chain;
+    }
+    
+private:
+    bool predecessor(string &pred, string &word) {
+        int n = pred.length();
+        int m = word.length();
+        
+        for (int i = 0, j = 0; i < n and j < m; ) {
+            if (pred[i] == word[j]) {
+                ++i;
+                ++j;
+            } else {
+                ++j;
+                if (j - i >= 2) {
+                    return false;
+                }
+            }
+        } 
+        
+        return true;
+    }
+};
+
+// or
+
+class Solution {
+public:
+    static bool compare(const string &s1, const string &s2) {
+        return s1.length() < s2.length();
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), compare);
+        unordered_map<string, int> dp;
+        int res = 0;
+        for (string w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                string pre = w.substr(0, i) + w.substr(i + 1);
+                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+            }
+            res = max(res, dp[w]);
+        }
+        return res;
+    }
+};
+
+
