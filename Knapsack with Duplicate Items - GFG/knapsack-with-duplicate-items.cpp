38ecef080e56d883006f0bb5c857aab5c8f458a4
rh-97 <52336887+rh-97@users.noreply.github.com>
2022-11-15 10:10:07 +0600

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -10,41 +10,41 @@ using namespace std;
 class Solution{
 public:
 
-    // int dp[1001][1001];
-    // Solution() {
-    //     memset(dp, -1, sizeof dp);
-    // }
+    int dp[1001][1001];
+    Solution() {
+        memset(dp, -1, sizeof dp);
+    }
     
 
-    // int knapSack(int N, int W, int val[], int wt[])
-    // {
-    //     if (dp[N][W] != -1) return dp[N][W];
-    //     if (W == 0 or N == 0) return 0;
-        
-    //     dp[N][W] = knapSack(N - 1, W, val, wt);
-    //     if (W >= wt[N-1])
-    //         dp[N][W] = max(dp[N][W], val[N-1]+knapSack(N, W - wt[N-1], val, wt));
-        
-    //     return dp[N][W];
-    // }   
-    
-    
     int knapSack(int N, int W, int val[], int wt[])
     {
-        int dp[N+1][W+1] = {0};
-        memset(dp, 0, sizeof dp);
+        if (dp[N][W] != -1) return dp[N][W];
+        if (W == 0 or N == 0) return 0;
         
-        for (int i = 1; i <= N; ++i) {
-            for (int j = 1; j <= W; ++j) {
-                dp[i][j] = dp[i-1][j];
-                if (j >= wt[i-1]) {
-                    dp[i][j] = max(dp[i][j], val[i-1] + dp[i][j - wt[i-1]]);
-                }
-            }
-        }
+        dp[N][W] = knapSack(N - 1, W, val, wt);
+        if (W >= wt[N-1])
+            dp[N][W] = max(dp[N][W], val[N-1]+knapSack(N, W - wt[N-1], val, wt));
         
         return dp[N][W];
-    }
+    }   
+    
+    
+    // int knapSack(int N, int W, int val[], int wt[])
+    // {
+    //     int dp[N+1][W+1];
+    //     memset(dp, 0, sizeof dp);
+        
+    //     for (int i = 1; i <= N; ++i) {
+    //         for (int j = 1; j <= W; ++j) {
+    //             dp[i][j] = dp[i-1][j];
+    //             if (j >= wt[i-1]) {
+    //                 dp[i][j] = max(dp[i][j], val[i-1] + dp[i][j - wt[i-1]]);
+    //             }
+    //         }
+    //     }
+        
+    //     return dp[N][W];
+    // }
     
     
     
